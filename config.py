77a37b8e3414f3a2d23fcbe168c0653cf6fe0f78
hiven <mail@hiven.com>
2023-03-04 09:48:23 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    APP_SEARCH_API_KEY = os.environ.get('APP_SEARCH_API_KEY')
-    APP_SEARCH_API_KEY_BLOG = os.environ.get('APP_SEARCH_API_KEY_BLOG')
-    APP_SEARCH_BASE_ENDPOINT = os.environ.get('APP_SEARCH_BASE_ENDPOINT') or 'localhost:3002/api/as/v1'
+    APP_SEARCH_API_KEY = os.environ.get('YOUR_MASTER_KEY_VALUE')
+    APP_SEARCH_API_KEY_BLOG = os.environ.get('YOUR_MASTER_KEY_VALUE')
+    APP_SEARCH_BASE_ENDPOINT = os.environ.get('APP_SEARCH_BASE_ENDPOINT') or 'http://109.205.61.211'
     APP_SEARCH_USE_HTTPS = os.environ.get('APP_SEARCH_USE_HTTPS') or 'False'
-    # APP_SEARCH_USE_HTTPS = False or False
     POSTS_PER_PAGE = 10
